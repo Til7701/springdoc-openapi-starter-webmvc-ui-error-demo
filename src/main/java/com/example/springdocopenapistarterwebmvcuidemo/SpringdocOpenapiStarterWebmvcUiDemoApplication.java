d3f80b1d2ef8f6133541c14b1c12c8ba58c13320
Tilman Holube <tilman@holube.de>
2025-06-10 20:35:33 +0200

    initial reproduction

---
+++ src/main/java/com/example/springdocopenapistarterwebmvcuidemo/SpringdocOpenapiStarterWebmvcUiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdocopenapistarterwebmvcuidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdocOpenapiStarterWebmvcUiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdocOpenapiStarterWebmvcUiDemoApplication.class, args);
+    }
+
+}
